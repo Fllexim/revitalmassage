d8478a78d88f3ce30091287328263be3b0d4ee5d
Fllexim <artem.ulashka@gmail.com>
2025-05-01 21:34:10 +0300

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("https://revital-massage.local/header.html")
+    fetch("https://fllexim.github.io/revitalmassage/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
