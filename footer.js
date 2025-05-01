1af8f465096f1a781f046be9dd91dd0570b91f7e
Fllexim <artem.ulashka@gmail.com>
2025-05-01 21:34:37 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("https://revital-massage.local/footer.html")
+    fetch("https://fllexim.github.io/revitalmassage/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
